54a9b1dce2db0598bb2c3a0a4edb8855cc5bde31
Micha <michaelaharin@gmail.com>
2021-05-04 13:39:54 +0200

    add jumping

--- js.js
+++ js.js
@@ -1,26 +1,42 @@
 document.addEventListener('keydown', (event) => {
     console.log(event.key);
 
+    let spritesheet = document.querySelector('.spritesheet');
+    let char = document.querySelector('.char');
+
     switch (event.key) {
         case'ArrowLeft':
-            document.querySelector('.spritesheet').setAttribute('direction', 'ArrowLeft');
+            spritesheet.setAttribute('direction', 'ArrowLeft');
             document.querySelectorAll('.parallax').forEach(element => {
                 console.log(window.getComputedStyle(element).backgroundPositionX);
-             element.style.setProperty('--startLeft', 2100-parseFloat(window.getComputedStyle(element).backgroundPositionX) + "px");
-             element.style.setProperty('--targetLeft','0');
+                // trying to make the direction change smoother
+                //     element.style.setProperty('--startLeft', 2100 - parseFloat(window.getComputedStyle(element).backgroundPositionX) + "px");
+                //     element.style.setProperty('--targetLeft', '0');
                 element.classList.remove('backgroundLeft');
                 element.classList.add('backgroundRight');
             })
             break;
         case'ArrowRight':
-            document.querySelector('.spritesheet').setAttribute('direction', 'ArrowRight');
+            spritesheet.setAttribute('direction', 'ArrowRight');
             document.querySelectorAll('.parallax').forEach(element => {
-             element.style.setProperty('--startLeft',2100-parseFloat(window.getComputedStyle(element).backgroundPositionX) + "px");
-             element.style.setProperty('--targetLeft','-2100px');
+                //     element.style.setProperty('--startLeft', 2100 - parseFloat(window.getComputedStyle(element).backgroundPositionX) + "px");
+                //     element.style.setProperty('--targetLeft', '-2100px');
                 element.classList.remove('backgroundRight');
                 element.classList.add('backgroundLeft');
             })
             break;
+        case'ArrowUp':
+
+        function jump() {
+            char.classList.remove('jump');
+            setTimeout(() => {
+                char.classList.add('jump')
+            }, 1)
+        }
+
+            jump();
+
     }
 })
+
 
